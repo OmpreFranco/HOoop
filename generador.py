e7d111abc40bd19f8252e94f804852b39df8a6c7
franco <franco@WTPC>
2017-03-02 17:25:02 -0300

    modifique el generador de ruido blanco en  generador.py

--- generador.py
+++ generador.py
@@ -17,14 +17,21 @@ class Generador(object):
     def generar(self, tiempo_inicial, tiempo_final):
 
         import math
-
+        import numpy as np
         cantidad_muestras = (tiempo_final - tiempo_inicial).seconds/\
         self.frecuencia_muestreo
 
         muestras = range(cantidad_muestras)
         #TODO agregar un ruido blanco a la senal
+#==============================================================================
+# Vamos a generar el ruido blanco
+        
+        
+#==============================================================================
 
-        ret = [self.amplitud*math.sin(2*(1/self.frecuencia)*i+self.fase) \
+        ret = [self.amplitud*math.sin(2*(1/self.frecuencia)*i+self.fase) + self.amplitud *np.random.uniform(-1,1)*0.001 \
         for i in muestras]
 
         return ret
+
+#-------------------
